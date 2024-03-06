27cc4f626855e2fe819997255f4afabd3c3af4df
hoang2807 <viethoang28072000@gmail.com>
2024-03-06 12:00:11 +0700

    first commit

---
+++ tailwind.config.mjs
@@ -0,0 +1,25 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+  theme: {
+    extend: {},
+  },
+  plugins: [require('daisyui')],
+  daisyui: {
+    themes: [
+      {
+        mytheme: {
+          "primary": "#00bdff",
+          "secondary": "#ff0000",
+          "accent": "#0000ff",
+          "neutral": "#2c2824",
+          "base-100": "#fff9ff",
+          "info": "#006dff",
+          "success": "#00c75b",
+          "warning": "#c98400",
+          "error": "#ff1163",
+        },
+      },
+    ],
+  },
+}
